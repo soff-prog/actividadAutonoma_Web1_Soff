2d41547fbedb3a1104aed4bbdfa4dad4d345dfbc
soff <sof.polette08@gmail.com>
2026-02-09 21:55:29 -0500

    justin

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,3 @@
 import { defineConfig } from 'vite'
-import { resolve } from 'path'
 
-export default defineConfig({
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-        contacto: resolve(__dirname, 'src/pages/contacto.html'),
-        nosotros: resolve(__dirname, 'src/pages/nosotros.html'),
-        historia: resolve(__dirname, 'src/pages/historia.html'),
-      }
-    }
-  }
-})
+export default defineConfig({})
